6703069e2a74769691a166c2a1e8b78282e2df28
Kasun Hewage <kasun.ch@gmail.com>
2020-12-16 00:23:46 +0100

    Adopted to the latest version of the pyrequires module

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,4 @@
 from conans import ConanFile, tools
-import re
 
 class QtSvgConan(ConanFile):
     name = "qtsvg"
@@ -9,17 +8,9 @@ class QtSvgConan(ConanFile):
     homepage = "https://code.qt.io/cgit/qt/qtsvg.git"
     license = "LGPL-3.0"  # SPDX Identifiers https://spdx.org/licenses/
 
-    python_requires = "qtmodulepyreq/0.1.0@blixt/testing"
+    python_requires = "qtmodulepyreq/0.1.0@blixt/stable"
     python_requires_extend = "qtmodulepyreq.QtModuleConanBase"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False]}
     default_options = {"shared": True}
-
-    def set_version(self):
-        git = tools.Git(folder=self.recipe_folder)
-        version = re.sub(".*/", "", str(git.get_branch()))
-        self.version = version
-
-    def requirements(self):
-        self.requires("qt/%s@bincrafters/stable" % self.version)
