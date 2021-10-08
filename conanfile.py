571733199f6899a0ec277400e4c27e3dff93dad0
Kasun Hewage <kasun.ch@gmail.com>
2021-10-08 10:23:05 +0200

    Changed to qtmodulepyreq version to 0.2.0

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class QtSvgConan(ConanFile):
     homepage = "https://code.qt.io/cgit/qt/qtsvg.git"
     license = "LGPL-3.0"  # SPDX Identifiers https://spdx.org/licenses/
 
-    python_requires = "qtmodulepyreq/0.1.0@blixt/stable"
+    python_requires = "qtmodulepyreq/0.2.0@blixt/stable"
     python_requires_extend = "qtmodulepyreq.QtModuleConanBase"
 
     settings = "os", "arch", "compiler", "build_type"
